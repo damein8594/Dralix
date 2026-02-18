60b35d16091f79fb26a5af80a98f24649cd911a6
Damien Higgins <ninjaboydamo@icloud.com>
2026-02-18 12:44:01 +0000

    fixed more bugs preventing deployment

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,7 @@ from flask import session
 flask_app = create_app()
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
-    
+    import os
+    port = int(os.environ.get("PORT", 5000))  # use Render's port if available
+    flask_app.run(host="0.0.0.0", port=port)
 
