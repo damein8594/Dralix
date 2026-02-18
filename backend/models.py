b0f71834b605c077422695818c0b2421cab81f57
Damien Higgins <ninjaboydamo@icloud.com>
2026-02-18 12:09:00 +0000

    changed my models.py to allow longer descriptions

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(45), nullable=False)
     status = db.Column(db.String(45), nullable=False)
-    description = db.Column(db.String(45), nullable=False)
+    description = db.Column(db.Text, nullable=False)
     storypoint = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
